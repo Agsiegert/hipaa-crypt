b8dc8ee18083eb9828e8bb6c83dd6e779fb95feb
Jason Waldrip <jason@waldrip.net>
2013-10-28 09:28:56 -0600

    finish encrypted object spec

---
+++ spec/lib/hipaa-crypt/encrypted_object_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe HipaaCrypt::EncryptedObject do
+
+  describe '#initialize' do
+    let(:value) { "value" }
+    let(:iv) { SecureRandom.hex }
+    subject(:instance) { described_class.allocate }
+    
+    it 'should set a value' do
+      expect { instance.send(:initialize, value, iv) }
+      .to change { instance.value }
+          .to value
+    end
+
+    it 'should set an iv' do
+      expect { instance.send(:initialize, value, iv) }
+      .to change { instance.iv }
+          .to iv
+    end
+  end
+
+end
