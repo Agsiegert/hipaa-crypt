b934cf680ea389030a5c90265a2b6d67eadc7d29
Jason Waldrip <jason@waldrip.net>
2013-10-28 17:00:00 -0600

    add attributes

--- lib/hipaa-crypt.rb
+++ lib/hipaa-crypt.rb
@@ -1,6 +1,7 @@
 module HipaaCrypt
 
   autoload :Encryptor, 'hipaa-crypt/encryptor'
+  autoload :Attributes, 'hipaa-crypt/attributes'
   autoload :EncryptedObject, 'hipaa-crypt/encrypted_object'
 
 end
