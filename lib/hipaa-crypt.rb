1d5969d0882e550e09d26e4c90850b5646d633d4
Jason Waldrip <jason@waldrip.net>
2013-10-29 10:08:37 -0600

    require callbacks

--- lib/hipaa-crypt.rb
+++ lib/hipaa-crypt.rb
@@ -1,5 +1,6 @@
 module HipaaCrypt
 
+  autoload :Callbacks, 'hipaa-crypt/callbacks'
   autoload :Encryptor, 'hipaa-crypt/encryptor'
   autoload :Attributes, 'hipaa-crypt/attributes'
   autoload :EncryptedObject, 'hipaa-crypt/encrypted_object'
