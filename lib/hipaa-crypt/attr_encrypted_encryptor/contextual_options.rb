82473320060fd76bf2c8358f0944bef9f36f57d4
Yun-Hui Fan <yfan@itriagehealth.com>
2013-11-01 13:56:48 -0600

    Added a simple encryptor to mimic the encryptor used by the attr_encrypted gem that we want to remove.

---
+++ lib/hipaa-crypt/attr_encrypted_encryptor/contextual_options.rb
@@ -0,0 +1,56 @@
+module HipaaCrypt
+  class OldEncryptor
+    class ContextualOptions
+
+      attr_reader :options
+
+      def initialize(options)
+        @options = options
+      end
+
+      def context
+        @context || raise(ArgumentError, 'context not set')
+      end
+
+      def get(key, &block)
+        normalize_object(options[key]) || (block.call if block_given?)
+      end
+
+      def with_context(context)
+        dup.tap { |options| options.instance_variable_set(:@context, context) }
+      end
+
+      def raw_value(key)
+        options[key]
+      end
+
+      protected
+
+      def normalize_object(object)
+        case object
+        when Symbol
+          normalize_symbol(object)
+        when Proc
+          normalize_proc(object)
+        else
+          object
+        end
+      end
+
+      private
+
+      def normalize_symbol(symbol)
+        context.send symbol
+      end
+
+      def normalize_proc(proc)
+        if proc.arity > 0
+          proc.call(context)
+        else
+          proc.call
+        end
+      end
+
+    end
+  end
+end
