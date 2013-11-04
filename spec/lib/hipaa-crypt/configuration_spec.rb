12d48e0c95ccf21925c27757049790206b29ecb9
garciadanny <dannygarcia.me@gmail.com>
2013-11-04 15:59:30 -0700

    Configuration#cipher

--- spec/lib/hipaa-crypt/configuration_spec.rb
+++ spec/lib/hipaa-crypt/configuration_spec.rb
@@ -8,6 +8,7 @@ describe HipaaCrypt::Configuration do
       before(:each){ stub_const('Rails', double(logger: rails_logger)) }
       it 'uses Rails.logger' do
         config = HipaaCrypt::Configuration.new
+
         expect(config.logger).to eq rails_logger
       end
     end
@@ -17,7 +18,28 @@ describe HipaaCrypt::Configuration do
       it 'should use a std out logger' do
         config = HipaaCrypt::Configuration.new
         expect(Logger).to receive(:new).with(STDOUT).and_return(fake_logger)
+
         expect(config.logger).to eq fake_logger
+      end
+    end
+  end
+
+  describe '#cipher' do
+    context 'when no cipher is added' do
+      it 'sets the default cipher' do
+        default_cipher = { name: 'AES', length: 256, mode: 'CBC' }
+
+        expect( HipaaCrypt::Configuration.new.cipher).to eq default_cipher
+      end
+    end
+
+    context 'when a cipher is added' do
+      it 'sets the  cipher' do
+        cipher = { name: 'XYZ', length: 256, mode: 'ABC' }
+        config = HipaaCrypt::Configuration.new
+        config.cipher = cipher
+
+        expect(config.cipher).to eq cipher
       end
     end
   end
