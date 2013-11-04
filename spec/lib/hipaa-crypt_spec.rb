b21471d30170be9d9cef3d8981f72d730d13a46d
garciadanny <dannygarcia.me@gmail.com>
2013-11-04 15:05:58 -0700

    specs for HipaaCrypt.config

--- spec/lib/hipaa-crypt_spec.rb
+++ spec/lib/hipaa-crypt_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe HipaaCrypt do
 
+  describe '.config' do
+    context 'when a cipher is added' do
+      it 'sets Configuration#chiper' do
+        cipher = { name: 'XYZ', length: 256, mode: 'ABC' }
+        config = HipaaCrypt.config { |c| c.cipher =  cipher }
+
+        expect(config.cipher).to eq cipher
+      end
+    end
+
+    context 'when a cipher is not added' do
+      it 'sets a default cipher' do
+        default_cipher = { name: 'AES', length: 256, mode: 'CBC' }
+        HipaaCrypt.instance_variable_set(:@config, nil)
+        config = HipaaCrypt.config
+
+        expect(config.cipher).to eq default_cipher
+      end
+    end
+
+    context 'when a key is added' do
+      it 'sets Configuration#key' do
+        key = 'a secret key'
+        config = HipaaCrypt.config { |c| c.key = key }
+
+        expect(config.key).to eq key
+      end
+    end
+  end
 end
