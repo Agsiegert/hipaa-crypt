5fb876893f340c91b166d0048e7c63ce2649ef7e
Jason Waldrip <jason@waldrip.net>
2013-11-05 08:57:22 -0700

    add new config options

--- lib/hipaa-crypt/configuration.rb
+++ lib/hipaa-crypt/configuration.rb
@@ -3,14 +3,21 @@ require 'logger'
 module HipaaCrypt
   class Configuration
     attr_accessor :key
-    attr_writer :cipher, :logger
+    attr_writer :cipher, :logger, :silent_re_encrypt
 
     def cipher
       @cipher ||= { name: :AES, key_length: 256, mode: :CBC }
     end
 
     def logger
-      (defined?(Rails) ? Rails.logger : Logger.new(STDOUT)).dup
+      begin
+        @logger || (defined?(Rails) ? Rails.logger : Logger.new(STDOUT))
+      end.dup
     end
+
+    def silent_re_encrypt
+      !!@silent_re_encrypt
+    end
+
   end
 end
