30c52f448bfbc15e19eacf8fea2d8c8d1b4cfdae
Jason Waldrip <jason@waldrip.net>
2013-11-05 08:56:41 -0700

    better re-encryption

--- lib/hipaa-crypt/attributes/active_record/class_methods.rb
+++ lib/hipaa-crypt/attributes/active_record/class_methods.rb
@@ -3,29 +3,51 @@ module HipaaCrypt
     module ActiveRecord
       module ClassMethods
 
-        def re_encrypt(*args)
-          find_in_batches do |group|
-            group.each do |instance|
-              begin
-                instance.re_encrypt(*args.dup)
-                instance.save
-              rescue Exception
-                instance
-              end
-            end
+        [:re_encrypt, :re_encrypt!].each do |method|
+          define_method method do |*args|
+            args = args.dup
+            query = re_encrypt_query_from_args(args)
+            query.re_encrypt_in_batches method, *args
           end
         end
 
-        def re_encrypt!(*args)
-          find_in_batches do |group|
-            group.each do |instance|
-              instance.re_encrypt(*args)
-              instance.save!
+        def re_encrypt_in_batches(method, *args)
+          success_count, fail_count = 0, 0
+          find_each do |instance|
+            if instance.send(method, *args.dup) && instance.save!(validate: false)
+              success_count += 1 and print_success
+            else
+              fail_count += 1 and print_fail
             end
           end
+
+          puts_counts(success_count, fail_count)
         end
 
         private
+
+        def print_fail
+          print "\e[0;31;49mF\e[0m" unless HipaaCrypt.config.silent_re_encrypt
+        end
+
+        def print_success
+          print "\e[0;32;49m.\e[0m" unless HipaaCrypt.config.silent_re_encrypt
+        end
+
+        def puts_counts(success_count, fail_count)
+          puts "Re-Encrypted \e[0;32;49m#{success_count}\e[0m #{name} records",
+               "\e[0;31;49m#{fail_count}\e[0m Failed" unless HipaaCrypt.config.silent_re_encrypt
+        end
+
+        def re_encrypt_query_from_args(args)
+          options = args.last.is_a?(Hash) ? args.pop : {}
+          ops     = { 'lt' => '<', 'gt' => '>' }
+          options.select { |key, value| key =~ /_(lt|gt)/ }.reduce(relation) do |rel, (quop, value)|
+            options.delete(quop)
+            op, attr = quop.to_s.reverse.split('_', 2).map(&:reverse)
+            rel.where "#{attr} #{ops[op]} ?", value
+          end.tap { args << options }
+        end
 
         def relation(*args)
           super(*args).tap do |relation|
@@ -65,3 +87,4 @@ module HipaaCrypt
     end
   end
 end
+
