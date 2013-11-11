c852313790a41a34f611bce241ce8b41c39d102d
Jason Waldrip <jason@waldrip.net>
2013-11-10 22:16:51 -0700

    fix the log_formatter constant

--- lib/hipaa-crypt/attributes/adapters/active_record/log_formatter.rb
+++ lib/hipaa-crypt/attributes/adapters/active_record/log_formatter.rb
@@ -1,17 +1,19 @@
 module HipaaCrypt
   module Attributes
-    module ActiveRecord
-      class LogFormatter
-        attr_reader :record
+    module Adapters
+      module ActiveRecord
+        class LogFormatter
+          attr_reader :record
 
-        def initialize(record)
-          @record = record
+          def initialize(record)
+            @record = record
+          end
+
+          def call(severity, time, progname, msg)
+            "#{severity.upcase} [#{time}] #<#{record.class.name} id: #{record.id}> #{msg}\n"
+          end
+
         end
-
-        def call(severity, time, progname, msg)
-          "#{severity.upcase} [#{time}] #<#{record.class.name} id: #{record.id}> #{msg}\n"
-        end
-
       end
     end
   end
