4dd5ac1d86f6adb337905adac630c4311dcc188b
Jason Waldrip <jason@waldrip.net>
2013-11-12 15:15:19 -0700

    with rescue

--- lib/hipaa-crypt/attr_encrypted_encryptor.rb
+++ lib/hipaa-crypt/attr_encrypted_encryptor.rb
@@ -4,24 +4,24 @@ module HipaaCrypt
   class AttrEncryptedEncryptor < Encryptor
 
     def decrypt string, iv = options.get(:iv)
-      setup_cipher __method__, iv
-      encrypted_value     = options.get(:encode) ? (decode string) : string
-      value               = cipher.update(encrypted_value) + cipher.final
-      value_for_callbacks = (options.get(:marshal) ? (deserialize value) : value)
-      Callbacks.new(options.raw_value :after_load).run value_for_callbacks
-    rescue Exception => exception
-      rescue_with_handler(exception) || raise(exception)
+      with_rescue do
+        setup_cipher __method__, iv
+        encrypted_value     = options.get(:encode) ? (decode string) : string
+        value               = cipher.update(encrypted_value) + cipher.final
+        value_for_callbacks = (options.get(:marshal) ? (deserialize value) : value)
+        Callbacks.new(options.raw_value :after_load).run value_for_callbacks
+      end
     end
 
     def encrypt value, iv = options.get(:iv) # Should return [string, iv]
-      processed_value = Callbacks.new(options.raw_value :before_encrypt).run value
-      value           = options.get(:marshal) ? serialize(processed_value) : processed_value
-      setup_cipher __method__, iv
-      encrypted_value = cipher.update(value) + cipher.final
-      value           = options.get(:encode) ? (encode encrypted_value) : encrypted_value
-      [value, iv]
-    rescue Exception => exception
-      rescue_with_handler(exception) || raise(exception)
+      with_rescue do
+        processed_value = Callbacks.new(options.raw_value :before_encrypt).run value
+        value           = options.get(:marshal) ? serialize(processed_value) : processed_value
+        setup_cipher __method__, iv
+        encrypted_value = cipher.update(value) + cipher.final
+        value           = options.get(:encode) ? (encode encrypted_value) : encrypted_value
+        [value, iv]
+      end
     end
 
     private
