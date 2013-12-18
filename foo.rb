09504fbfc570c7ed9f49ff699b45b7888b57cb83
Jason Waldrip <jason@waldrip.net>
2013-12-17 17:37:03 -0700

    a sample

--- foo.rb
+++ foo.rb
@@ -7,7 +7,19 @@ puts HipaaCrypt.config.logger
 
 class Foo
   include HipaaCrypt::Attributes
+
+  # attr_accessor :encrypted_foo
+
   encrypt :foo, key: SecureRandom.hex, cipher: 'aes-256-cbc'
+
+  def foo=(val)
+    @my_amazing_foo = val
+  end
+
+  def foo
+    @my_amazing_foo
+  end
+
 end
 
 f = Foo.new
