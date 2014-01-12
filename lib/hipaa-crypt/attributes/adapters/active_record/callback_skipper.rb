3bf8c4c178b049139ef36c884d4617e1b6b852b3
Jason Waldrip <jason@waldrip.net>
2014-01-12 14:05:54 -0700

    remove another smell

--- lib/hipaa-crypt/attributes/adapters/active_record/callback_skipper.rb
+++ lib/hipaa-crypt/attributes/adapters/active_record/callback_skipper.rb
@@ -6,13 +6,16 @@ module HipaaCrypt
 
           # Invoke a save without executing any callbacks.
           def save_without_callbacks
-            changed_attrs = changed.reduce({}) do |hash, attr|
-              hash.merge attr => read_attribute(attr)
-            end
-            changed_attrs.blank? || 1 == self.class.unscoped.where(self.class.primary_key => id).update_all(changed_attrs)
+            !changed? || 1 == self.class.unscoped.where(self.class.primary_key => id).update_all(get_changed_attributes)
           rescue => e
             HipaaCrypt.logger.error "Re-Encrypt Error => #{e.class}: #{e.message}"
             false
+          end
+
+          def get_changed_attributes
+            changed.reduce({}) do |hash, attr|
+              hash.merge attr => read_attribute(attr)
+            end
           end
 
         end
